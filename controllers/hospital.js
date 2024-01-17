c4732f7a92ada308874f7e212ac2aa8f98b616ef
ideepakrajput <deepakkumar18112001@gmail.com>
2024-01-17 13:27:24 +0530

    id handle

--- controllers/hospital.js
+++ controllers/hospital.js
@@ -2,6 +2,14 @@ import connection from "../database.js";
 
 export const getHospitalData = async (req, res) => {
     const { id } = req.body;
+
+    if (!id || id.trim() == "") {
+        return res.status(400).json({
+            status: 400,
+            message: `req body doesn't get id !`
+        })
+    }
+
     const sql = `SELECT     
     H.name AS HospitalName,     
     COUNT(DISTINCT P.id) AS TotalPsychiatristCount,          
@@ -33,12 +41,12 @@ export const getHospitalData = async (req, res) => {
             message: `${err}`
         })
         if (results.length === 0) {
-            res.status(202).json({
+            return res.status(202).json({
                 status: 202,
                 message: "Data not found !"
             })
         } else {
-            res.status(200).json(results);
+            return res.status(200).json(results);
         }
     })
 }
